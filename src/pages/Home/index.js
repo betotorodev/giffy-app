d25d20f725e8063b41ab07c970f52faf6ed6cc62
Beto Toro <eoliveros@coca-cola.com>
2021-03-26 11:57:53 -0500

    [UPDATE] add useCallback, memo and good practicies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,22 @@
-import React, {useState} from "react"
+import React, { useCallback } from "react"
 import { useLocation } from "wouter"
 import ListOfGifs from 'components/ListOfGifs'
+import SearchForm from '../../components/SearchForm/index'
 import {useGifs} from 'hooks/useGifs'
 import LazyTrading from 'components/TrendingSearcher'
 
 export default function Home() {
-  const [keyword, setKeyword] = useState('')
   const [path, pushLocation] = useLocation()
   const {gifs} = useGifs()
 
-  const handleSubmit = evt => {
-    evt.preventDefault()
+  const handleSubmit = useCallback(({ keyword }) => {
     // navegar a otra ruta
     pushLocation(`/search/${keyword}`)
-  }
-
-  const handleChange = evt => {
-    setKeyword(evt.target.value)
-  }
+  }, [pushLocation])
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
-      </form>
+      <SearchForm onSubmit={handleSubmit}/>
       <div className="App-main">
         <div className="App-results">
           <h3 className="App-title">Última búsqueda</h3>
